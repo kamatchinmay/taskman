f61e2ebcba7555cfff649b0bb7279ee27e22ad24
ChinmayKamat <kamatchinmay@gmail.com>
2016-07-06 08:18:34 +0530

    improving client model

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Individual
 
 # Register your models here.
+admin.site.register(Individual)
--- clients/models.py
+++ clients/models.py
@@ -3,22 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Client(models.Model):
-    CLIENT_TYPES = (
-        ('individual','Individual'),
-        ('llp','Limited Liability Partnership'),
-        ('opc','One Person Company'),
-        ('partnership','Partnership Firm'),
-        ('pvt_ltd','Private Limited Company'),
-        ('pub_ltd','Public Limited Company'),
-        ('trust','Trust'),
-    )
 
-    PAN = models.CharField(max_length=10, blank=False, null=False)
-    DOB = models.DateField()
-    Type = models.CharField(max_length=11, null=False, choices=CLIENT_TYPES)
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-    comments = models.TextField(max_length=1000)
+    PAN = models.CharField(max_length = 10, blank = False, null = False, verbose_name = "PAN")
+    TAN = models.CharField(max_length = 10, blank = True, null = True, verbose_name = "TAN")
+    STN = models.CharField(max_length = 15, blank = True, null = True, verbose_name = "Service Tax Regn. No.")
+    CExNo = models.CharField(max_length = 15, blank = True, null = True, verbose_name = "Central Excise Regn. No.")
+    DOB = models.DateField(verbose_name = "Date of Birth")
+    created = models.DateTimeField(auto_now_add = True)
+    modified = models.DateTimeField(auto_now = True)
+    comments = models.TextField(max_length = 1000, verbose_name = "User Comments")
 
     class Meta:
         abstract = True
@@ -30,7 +23,9 @@ class Individual(Client):
         ('f','Female'),
     )
 
-    First_Name = models.CharField(max_length=20, blank=False, null=False)
-    Middle_Name = models.CharField(max_length=20, blank=True, null=True)
-    Last_Name = models.CharField(max_length=20, blank=False, null=False)
-    Gender = models.CharField(max_length=1, null=False, choices=GENDER_TYPES)
+    Type = "Individual"
+
+    First_Name = models.CharField(max_length = 20, blank = False, null = False, verbose_name = "First Name")
+    Middle_Name = models.CharField(max_length = 20, blank = True, null = True, verbose_name = "Middle Name")
+    Last_Name = models.CharField(max_length = 20, blank = False, null = False, verbose_name = "Last Name")
+    Gender = models.CharField(max_length = 1, null = False, choices = GENDER_TYPES, verbose_name = "Gender")
