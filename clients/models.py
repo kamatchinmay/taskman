cfd1fd7b5c950a5bf2e6b2d02da81b3198254d94
ChinmayKamat <kamatchinmay@gmail.com>
2016-07-06 08:53:45 +0530

    Almost set up the indicidual client model

--- clients/models.py
+++ clients/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 class Client(models.Model):
 
-    PAN = models.CharField(max_length = 10, blank = False, null = False, verbose_name = "PAN")
+    PAN = models.CharField(max_length = 10, blank = False, null = False, verbose_name = "PAN", help_text="Mandatory")
     TAN = models.CharField(max_length = 10, blank = True, null = True, verbose_name = "TAN")
     STN = models.CharField(max_length = 15, blank = True, null = True, verbose_name = "Service Tax Regn. No.")
     CExNo = models.CharField(max_length = 15, blank = True, null = True, verbose_name = "Central Excise Regn. No.")
-    DOB = models.DateField(verbose_name = "Date of Birth")
+    DOB = models.DateField(verbose_name = "Date of Birth / Registration", help_text="Mandatory")
     created = models.DateTimeField(auto_now_add = True)
     modified = models.DateTimeField(auto_now = True)
     comments = models.TextField(max_length = 1000, verbose_name = "User Comments")
@@ -23,9 +23,7 @@ class Individual(Client):
         ('f','Female'),
     )
 
-    Type = "Individual"
-
-    First_Name = models.CharField(max_length = 20, blank = False, null = False, verbose_name = "First Name")
+    First_Name = models.CharField(max_length = 20, blank = False, null = False, verbose_name = "First Name", help_text="Mandatory")
     Middle_Name = models.CharField(max_length = 20, blank = True, null = True, verbose_name = "Middle Name")
-    Last_Name = models.CharField(max_length = 20, blank = False, null = False, verbose_name = "Last Name")
-    Gender = models.CharField(max_length = 1, null = False, choices = GENDER_TYPES, verbose_name = "Gender")
+    Last_Name = models.CharField(max_length = 20, blank = False, null = False, verbose_name = "Last Name", help_text="Mandatory")
+    Gender = models.CharField(max_length = 1, null = False, choices = GENDER_TYPES, verbose_name = "Gender", help_text="Mandatory")
