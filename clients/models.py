a89e9cffc9bed1f9acbb686606b9e0a08347240a
ChinmayKamat <kamatchinmay@gmail.com>
2016-07-07 07:17:00 +0530

    added uniquiness constraint on fields like pan tan stn CExn cin llpin etc

--- clients/models.py
+++ clients/models.py
@@ -5,13 +5,13 @@ from django.db import models
 class Client(models.Model):
 
     PAN = models.CharField(max_length = 10, blank = False, null = False,
-                           verbose_name = "PAN", help_text="Mandatory")
+                           verbose_name = "PAN", help_text="Mandatory", unique=True)
     TAN = models.CharField(max_length = 10, blank = True, null = True,
-                           verbose_name = "TAN")
+                           verbose_name = "TAN", unique=True)
     STN = models.CharField(max_length = 15, blank = True, null = True,
-                           verbose_name = "Service Tax Regn. No.")
+                           verbose_name = "Service Tax Regn. No.",unique=True)
     CExNo = models.CharField(max_length = 15, blank = True, null = True,
-                             verbose_name = "Central Excise Regn. No.")
+                             verbose_name = "Central Excise Regn. No.", unique=True)
     DOB = models.DateField(verbose_name = "Date of Birth / Registration",
                            help_text="Mandatory")
     created = models.DateTimeField(auto_now_add = True)
@@ -35,13 +35,14 @@ class Individual(Client):
                               choices = GENDER_TYPES, verbose_name = "Gender", help_text="Mandatory")
 
 class Partnership(Client):
-    Partnership_Name = models.CharField(max_length = 80, blank = False, null = False, verbose_name = "LLP Name", help_text="Mandatory")
+    Partnership_Name = models.CharField(max_length = 80, blank = False, null = False,
+                                        verbose_name = "LLP Name", help_text="Mandatory", unique=True)
 
 class LLP(Client):
     LLP_Name = models.CharField(max_length = 80, blank = False, null = False,
-                                verbose_name = "LLP Name", help_text="Mandatory")
+                                verbose_name = "LLP Name", help_text="Mandatory", unique=True)
     LLPIN = models.CharField(max_length = 8, blank = False, null = False,
-                             verbose_name = "LLP Identification Number", help_text="Mandatory")
+                             verbose_name = "LLP Identification Number", help_text="Mandatory", unique=True)
 
 class Limited_Company(Client):
     COMPANY_TYPES = (
@@ -51,8 +52,9 @@ class Limited_Company(Client):
     )
 
     Company_Name = models.CharField(max_length = 120, blank = False, null = False,
-                                    verbose_name = "Company Name", help_text="Mandatory")
+                                    verbose_name = "Company Name", help_text="Mandatory",unique=True)
     Company_CIN = models.CharField(max_length = 21, blank = False, null = False,
-                                   verbose_name = "Company CIN", help_text="Mandatory")
+                                   verbose_name = "Company CIN", help_text="Mandatory", unique=True)
     Company_Type = models.CharField(max_length = 3, blank = False, null = False,
-                                    choices = COMPANY_TYPES, verbose_name = "Company Type", help_text="Mandatory", default="pvt")
+                                    choices = COMPANY_TYPES, verbose_name = "Company Type",
+                                    help_text="Mandatory", default="pvt")
